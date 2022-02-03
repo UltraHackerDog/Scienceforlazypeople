71b641fbe596cfb04bc7c3dd75f134782c5ea196
UltraDog <69469132+UltraHackerDog@users.noreply.github.com>
2022-02-03 12:49:48 +0100

    rewrote the entire code base
    Made it possible for the user to input only 4 or even 6 items into results

--- main.py
+++ main.py
@@ -6,63 +6,30 @@ independent = input("What is your independent variable?\nindependent(unit):  ")
 dependent = input("What is your dependent variable?\ndependent(unit): ")
 intervals = [int(x) for x in input(f"\nWhich {independent} values are you measuring? \nPlease seperate your answers with commas.: ").split(', ')]
 
-try:
-    intervals[0]
-except IndexError:
-    raise IndexError(f"PLease enter at least 5 {independent} values") from None
-
-results1 = [int(x) for x in input(f"What are the results of {intervals[0]}: ").split(', ')]
-
-try:
-    intervals[1]
-except IndexError:
-    raise IndexError(f"PLease enter at least 5 {independent} values") from None
-
-results2 = [int(x) for x in input(f"What are the results of {intervals[1]}: ").split(', ')]
-
-try:
-    intervals[2]
-except IndexError:
-    raise IndexError(f"PLease enter at least 5 {independent} values") from None
-
-results3 = [int(x) for x in input(f"What are the results of {intervals[2]}: ").split(', ')]
-
-try:
-    intervals[3]
-except IndexError:
-    raise IndexError(f"PLease enter at least 5 {independent} values") from None
-
-results4 = [int(x) for x in input(f"What are the results of {intervals[3]}: ").split(', ')]
-
-try:
-    intervals[4]
-except IndexError:
-    raise IndexError(f"PLease enter at least 5 {independent} values") from None
-
-results5 = [int(x) for x in input(f"What are the results of {intervals[4]}: ").split(', ')]
+results = []
+for interval in intervals:
+    results.append([int(x) for x in input(f"What are the results of {intervals[0]}: ").split(', ')])
 
 # processing data
 def average(a):
     return mean(a)
 
-def range(list):
+def valrange(list):
     list.sort()
     min = list[0]
     max = list[-1]
     return max - min
 
 
-print(f"\nThe average of {intervals[0]} is {average(results1)}. The Range is {range(results1)}")
-print(f"\nThe average of {intervals[1]} is {average(results2)}. The Range is {range(results2)}")
-print(f"\nThe average of {intervals[2]} is {average(results3)}. The Range is {range(results3)}")
-print(f"\nThe average of {intervals[3]} is {average(results4)}. The Range is {range(results4)}")
-print(f"\nThe average of {intervals[4]} is {average(results5)}. The Range is {range(results5)}")
+for result in results:
+    print(f"The average of {result} is {average(result)} and the range is {valrange(result)}")
+    print("-" * 50)
 
 # graphing
 
 y = np.array(intervals)
-x = np.array([average(results1), average(results2), average(results3), average(results4), average(results5)])
-xerrorbars = [range(results1) / 2, range(results2) / 2, range(results3) / 2, range(results4) / 2, range(results5) / 2]
+x = np.array([mean(result) for result in results])
+xerrorbars = [valrange(result) / 2 for result in results]
 
 def give_me_a_straight_line(x,y):
     w, b  = np.polyfit(x,y,deg=1)

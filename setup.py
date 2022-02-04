a2988f8c95e5ac10e2b39a34c5d1c34a3b7abd33
UltraDog <69469132+UltraHackerDog@users.noreply.github.com>
2022-02-04 10:26:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-APP = ['main.py']
-OPTIONS = {}
+APP = ['Sandwich.py']
+DATA_FILES = []
+OPTIONS = {'argv_emulation': True}
 
 setup(
     app=APP,
+    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
